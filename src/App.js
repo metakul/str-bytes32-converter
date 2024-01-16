967fd87c6cd51df60ecbe6407a76e6296d0dd9e0
Shubham Kunwar <kshubham@c3ihub.iitk.ac.in>
2024-01-16 14:11:02 +0530

    add color theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { ColorModeContext, useMode, themes } from "./themes/themes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-function App() {
+export default function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <HelmetProvider>
+          HI METAKUL
+      </HelmetProvider>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+
   );
 }
-
-export default App;
